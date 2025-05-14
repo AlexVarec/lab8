1832115735e2efe7c0d51d0df40f91e56cec556d
AlexVarec <aleksandrasvarec@gmail.com>
2025-05-14 16:22:13 +0300

    Add HTML output

--- my_functions.py
+++ my_functions.py
@@ -1,12 +1,17 @@
+""""""
+
 #Task 1: Testing a Calculator (Addition & Subtraction)
 def add(a, b):
+    """Return the sum of a and b"""
     return a + b
 
 def subtract(a, b):
+    """Return the subtraction of a and b"""
     return a - b 
 
 #Task 2: Testing an Even Number Checker
 def is_even(n):
+    """Returning True if n is even, False otherwise"""
     if n % 2 == 0:
         return True
     else:
@@ -14,10 +19,12 @@ def is_even(n):
 
 #Task 3: Testing a String Reversal Function
 def reverse_string(s):
+     """Return reverse of input string s"""
      return s[::-1]
 
 #Task 4: Testing a Palindrome Checker
 def is_palindrome(s):
+    """Return True if s is palindrome, False otherwise"""
     s = s.lower()  
     if s == s[::-1]:
         return True
@@ -26,10 +33,12 @@ def is_palindrome(s):
     
 #Task 5: Testing a Maximum Finder in a List
 def find_max(numbers):
+    """Return the maximum number"""
     return max(numbers)
 
 # ask 6: Testing a Factorial Function 
 def factorial(n):
+    """Return factorial of non negative intiger n"""
     if n < 0:
         raise ValueError("F is not defined")
     value = 1
@@ -39,10 +48,12 @@ def factorial(n):
     
 #Task 7: Testing a Number Sorting Function
 def sort_numbers(numbers):
+    """Return a sorted list of numbers"""
     return sorted(numbers)
 
 #Task 8: Testing a Password Validation Function
 def is_valid_password(password):
+    """Return false if the password is not valid"""
     if len(password) < 8:
         return False
     has_upper = any(c.isupper() for c in password)
